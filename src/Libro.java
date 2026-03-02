97ea3643aefdb70d221a709043921fac3b85214b
Francisco Remiro <fremigom@myuax.com>
2026-03-02 11:58:21 +0100

    feat (Libro): Creada la clase Libro

--- src/Libro.java
+++ src/Libro.java
@@ -1,4 +1,66 @@
-package PACKAGE_NAME;
-
 public class Libro {
+
+    private int id;
+    private String titulo;
+    private String autor;
+    private int numPaginas;
+
+    /**
+     * Metodo constructor de Libros
+     * @param id identificador del líbro
+     * @param titulo título del líbro
+     * @param autor autor del líbro
+     * @param numPaginas número de páginas del líbro
+     */
+    public Libro(int id, String titulo, String autor, int numPaginas) {
+        int numHojas;
+
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numPaginas = numPaginas;
+
+        numHojas = numPaginas/2;
+    }
+
+    /**
+     * Devuelve el id del líbro
+     * @return
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Devuelve el título del líbro
+     * @return
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * Devuelve el Autor del líbro
+     * @return
+     */
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Libro: {");
+
+        sb.append(String.format("\n id: %d; ", id));
+        sb.append(String.format(" titulo: %s; ", titulo));
+        sb.append(String.format(" autor: %s; ", autor));
+        sb.append(String.format(" numPaginas: %d; ", numPaginas));
+        sb.append("}");
+
+        return sb.toString();
+    }
 }
