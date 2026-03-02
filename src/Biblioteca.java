69cd31084b13e95f5f1b99b308a6b0d42fccc2b8
Francisco Remiro <fremigom@myuax.com>
2026-03-02 11:59:10 +0100

    feat (Biblioteca): Creada la clase Biblioteca con constructor y el método findLibro

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Biblioteca {
+
+    private Libro[] catalogo;
+    private final int CANT_MAX_LIBROS;
+
+    public Biblioteca(int cantidad_maxima_libros) {
+        CANT_MAX_LIBROS = cantidad_maxima_libros;
+
+        catalogo = new Libro[CANT_MAX_LIBROS];
+    }
+
+    /**
+     * Busca el primer libro que encuentre con el id recibido
+     * @param idlibro
+     * @return
+     */
+    public Libro findLibro(int idlibro){
+        boolean seguirBuscnado = true;
+        Libro libroencontrado = null;
+
+        for (int i = 0; i < CANT_MAX_LIBROS && seguirBuscnado; i++) {
+            if (catalogo[i] != null && catalogo[i].getId() == idlibro) {
+                seguirBuscnado = false;
+                libroencontrado = catalogo[i];
+            }
+        }
+
+        return libroencontrado;
+    }
+
+
+
+
+
 }
